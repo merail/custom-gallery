258e99af1040578ff9bca3831887c98da1f8583f
merail <rail.me@google.com>
2022-02-15 00:46:02 +0300

    Using Request Permissions Tool for handling READ_EXTERNAL_STORAGE permission

--- app/src/main/java/me/rail/customgallery/main/MainActivity.kt
+++ app/src/main/java/me/rail/customgallery/main/MainActivity.kt
@@ -1,21 +1,15 @@
 package me.rail.customgallery.main
 
 import android.Manifest
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
-import android.provider.Settings
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
-import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import me.rail.customgallery.R
 import me.rail.customgallery.databinding.ActivityMainBinding
+import me.rail.customgallery.main.permission.GoingToSettingsSnackbar
+import me.rail.customgallery.main.permission.RuntimePermissionRequester
 import me.rail.customgallery.media.MediaHandler
 import me.rail.customgallery.screens.main.MainFragment
 import javax.inject.Inject
@@ -29,11 +23,27 @@ class MainActivity: AppCompatActivity() {
     @Inject
     lateinit var navigator: Navigator
 
+    private lateinit var runtimePermissionRequester: RuntimePermissionRequester
+    private var permissions = Array(1) { Manifest.permission.READ_EXTERNAL_STORAGE }
+    private lateinit var notGrantedPermissions: ArrayList<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        checkReadExternalStoragePermission()
+        setOnGetPermissionsClickListener()
+
+        runtimePermissionRequester = RuntimePermissionRequester(this)
+
+        if (!runtimePermissionRequester.checkSelfPermissions(permissions)) {
+            runtimePermissionRequester.requestPermissions()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        checkRuntimePermissions()
     }
 
     override fun onRequestPermissionsResult(
@@ -42,49 +52,60 @@ class MainActivity: AppCompatActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        handleRequestCode(requestCode, grantResults)
-    }
+        notGrantedPermissions = runtimePermissionRequester.onRequestPermissionsResult(
+            requestCode,
+            permissions,
+            grantResults
+        )
 
-    private fun checkReadExternalStoragePermission() {
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            if (isPermissionErrorVisible()) {
-                hidePermissionError()
-            }
+        setGetRuntimePermissionsVisibility(notGrantedPermissions.isEmpty())
+        if (notGrantedPermissions.isEmpty()) {
             showMedia()
-        } else {
-            ActivityCompat.requestPermissions(
-                this, Array(1) { Manifest.permission.READ_EXTERNAL_STORAGE },
-                REQUEST_CODE_READ_EXTERNAL_STORAGE_PERMISSION
-            )
         }
     }
 
-    private fun handleRequestCode(requestCode: Int, grantResults: IntArray) {
-        when (requestCode) {
-            REQUEST_CODE_READ_EXTERNAL_STORAGE_PERMISSION -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    showMedia()
-                } else {
-                    handleRequestReadExternalStoragePermissionDenial()
-                }
-                return
-            }
+    private fun setOnGetPermissionsClickListener() {
+        binding.requestPermission.setOnClickListener { view: View ->
+            checkPermissionsForRationale()
+            checkDeniedPermissions(view)
         }
     }
 
-    private fun handleRequestReadExternalStoragePermissionDenial() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(
-                this,
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            )
-        ) {
-            showPermissionError()
+    private fun checkRuntimePermissions() {
+        val isAllRuntimePermissionsGranted =
+            runtimePermissionRequester.checkSelfPermissions(permissions)
+        setGetRuntimePermissionsVisibility(isAllRuntimePermissionsGranted)
+        if (isAllRuntimePermissionsGranted) {
+            showMedia()
+        }
+    }
+
+    private fun setGetRuntimePermissionsVisibility(hide: Boolean) {
+        if (hide) {
+            binding.permissionErrorContainer.visibility = View.GONE
         } else {
-            showGoingToSettingsSnackbar()
+            binding.permissionErrorContainer.visibility = View.VISIBLE
+        }
+    }
+
+    private fun checkPermissionsForRationale() {
+        val permissionsForRationale =
+            runtimePermissionRequester.getPermissionsForRationale(notGrantedPermissions)
+        if (permissionsForRationale.isNotEmpty()) {
+            runtimePermissionRequester.setPermissionsForRequest(permissionsForRationale)
+            runtimePermissionRequester.requestPermissions()
+        }
+    }
+
+    private fun checkDeniedPermissions(view: View) {
+        val deniedPermissions =
+            runtimePermissionRequester.getDeniedPermissions(notGrantedPermissions)
+        if (deniedPermissions.isNotEmpty()) {
+            val goingToSettingsSnackbar = GoingToSettingsSnackbar(this, view)
+            goingToSettingsSnackbar.showSnackbar(
+                "You must grant permissions in Settings!",
+                "Settings"
+            )
         }
     }
 
@@ -93,41 +114,5 @@ class MainActivity: AppCompatActivity() {
         mediaHandler.findMedia(applicationContext)
 
         navigator.replaceFragment(R.id.container, MainFragment())
-    }
-
-    private fun showPermissionError() {
-        binding.permissionErrorContainer.visibility = View.VISIBLE
-        binding.requestPermission.setOnClickListener {
-            checkReadExternalStoragePermission()
-        }
-    }
-
-    private fun showGoingToSettingsSnackbar() {
-        val goingToSettingsSnackbar = Snackbar.make(
-            binding.root,
-            resources.getString(R.string.instruction_for_read_external_storage_permission),
-            Snackbar.LENGTH_LONG
-        )
-        goingToSettingsSnackbar.setAction(
-            resources.getString(R.string.settings)
-        ) {
-            val intent = Intent()
-            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-            val uri =
-                Uri.fromParts("package", packageName, null)
-            intent.data = uri
-            startActivity(intent)
-        }
-        goingToSettingsSnackbar.show()
-    }
-
-    private fun hidePermissionError() {
-        binding.permissionErrorContainer.visibility = View.GONE
-    }
-
-    private fun isPermissionErrorVisible() = binding.permissionErrorContainer.isVisible
-
-    companion object {
-        private const val REQUEST_CODE_READ_EXTERNAL_STORAGE_PERMISSION = 1
     }
 }
